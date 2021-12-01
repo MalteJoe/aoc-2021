22916f1329df849a092818be4655c8f3fe868c49
Malte Jörgens <malte.joergens@wps.de>
2021-12-01 08:01:40 +0100

    01: Added Comments

--- src/main/day01/Day01.kt
+++ src/main/day01/Day01.kt
@@ -7,6 +7,9 @@ typealias Output = Int
 
 fun mapInput(lines: Sequence<String>): Input = lines.map(String::toInt).toList()
 
+/**
+ * **How many measurements are larger than the previous measurement?**
+ */
 fun part1(input: Input): Output {
     var increases = 0
     input.windowed(2).forEach { (a, b) ->
@@ -17,6 +20,9 @@ fun part1(input: Input): Output {
     return increases
 }
 
+/**
+ * Consider sums of a three-measurement sliding window. **How many sums are larger than the previous sum?**
+ */
 fun part2(input: Input): Output {
     var sumOfPreviousWindow: Int? = null
     var increases = 0
@@ -30,6 +36,9 @@ fun part2(input: Input): Output {
     return increases
 }
 
+/**
+ * [Sonar Sweep](https://adventofcode.com/2021/day/1)
+ */
 fun main() {
     val input = readInput("day01", ::mapInput)
     println(part1(input))
