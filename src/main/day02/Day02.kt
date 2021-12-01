b169fc34a1dee1280677bd49d010ee9313f6a020
Malte Jörgens <malte.joergens@wps.de>
2021-12-01 07:52:33 +0100

    02: Template

---
+++ src/main/day02/Day02.kt
@@ -0,0 +1,24 @@
+package day02
+
+import readInput
+
+typealias Input = List<Int>
+typealias Output = Int
+
+fun mapInput(lines: Sequence<String>): Input = TODO()
+
+fun part1(input: Input): Output {
+    TODO()
+}
+
+fun part2(input: Input): Output {
+    TODO()
+}
+
+fun main() {
+    val input = readInput("day02", ::mapInput)
+    println(part1(input))
+    println(part2(input))
+}
+
+
---
+++ src/test/day02/Day02Test.kt
@@ -0,0 +1,26 @@
+package day02
+
+import kotlin.test.*
+
+internal class Day02Test {
+
+    private val sampleInput: String = TODO()
+
+
+    @Test
+    fun parseInput() {
+        assertEquals(TODO(), mapInput(sampleInput.lineSequence()))
+    }
+
+    @Test
+    fun `part1 example`() {
+        val input = mapInput(sampleInput.lineSequence())
+        assertEquals(TODO(), part1(input))
+    }
+
+    @Test
+    fun `part2 example`() {
+        val input = mapInput(sampleInput.lineSequence())
+        assertEquals(TODO(), part2(input))
+    }
+}
