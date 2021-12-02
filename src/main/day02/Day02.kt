e73a3466b7143fbeef785c05fb7bd6282e29febf
Malte Jörgens <malte.joergens@wps.de>
2021-12-02 06:57:26 +0100

    02: Parsing + Part 1

--- src/main/day02/Day02.kt
+++ src/main/day02/Day02.kt
@@ -2,13 +2,31 @@ package day02
 
 import readInput
 
-typealias Input = List<Int>
+typealias Input = List<Command>
 typealias Output = Int
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+data class Command(val instruction: String, val units: Int)
 
+fun mapInput(lines: Sequence<String>): Input = lines.map { line ->
+    val (instruction, units) = line.split(' ')
+    Command(instruction, units.toInt())
+}.toList()
+
+/**
+ * Calculate the horizontal position and depth you would have after following the planned course.
+ * **What do you get if you multiply your final horizontal position by your final depth?**
+ */
 fun part1(input: Input): Output {
-    TODO()
+    var horizontalPos = 0
+    var depth = 0
+    input.forEach {
+        when (it.instruction) {
+            "forward" -> horizontalPos += it.units
+            "down" -> depth += it.units
+            "up" -> depth -= it.units
+        }
+    }
+    return horizontalPos * depth
 }
 
 fun part2(input: Input): Output {
@@ -16,7 +34,7 @@ fun part2(input: Input): Output {
 }
 
 /**
- * [TODO](https://adventofcode.com/2021/day/2)
+ * [Dive!](https://adventofcode.com/2021/day/2)
  */
 fun main() {
     val input = readInput("day02", ::mapInput)
--- src/test/day02/Day02Test.kt
+++ src/test/day02/Day02Test.kt
@@ -4,18 +4,32 @@ import kotlin.test.*
 
 internal class Day02Test {
 
-    private val sampleInput: String = TODO()
+    private val sampleInput: String = """forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"""
 
 
     @Test
     fun parseInput() {
-        assertEquals(TODO(), mapInput(sampleInput.lineSequence()))
+        assertEquals(
+            listOf(
+                Command("forward", 5),
+                Command("down", 5),
+                Command("forward", 8),
+                Command("up", 3),
+                Command("down", 8),
+                Command("forward", 2),
+            ), mapInput(sampleInput.lineSequence())
+        )
     }
 
     @Test
     fun `part1 example`() {
         val input = mapInput(sampleInput.lineSequence())
-        assertEquals(TODO(), part1(input))
+        assertEquals(150, part1(input))
     }
 
     @Test
