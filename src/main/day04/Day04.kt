a6a68db01a0d7875b5d8fbb135f6b6809bb415cb
Malte Jörgens <malte.joergens@wps.de>
2021-12-02 22:24:03 +0100

    04: Template

---
+++ src/main/day04/Day04.kt
@@ -0,0 +1,34 @@
+package day04
+
+import readInput
+
+typealias Input = Any // TODO
+typealias Output = Int
+
+/**
+ * [TODO](https://adventofcode.com/2021/day/4)
+ */
+fun main() {
+    val input = readInput("day04", ::mapInput)
+    println(part1(input))
+    println(part2(input))
+}
+
+
+fun mapInput(lines: Sequence<String>): Input = TODO()
+
+/**
+ * TODO
+ */
+fun part1(input: Input): Output {
+    TODO()
+}
+
+/**
+ * TODO
+ */
+fun part2(input: Input): Output {
+    TODO()
+}
+
+
