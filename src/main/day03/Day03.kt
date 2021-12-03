aaac31fd2c6bbd67f4177908c992f7bd78b232c6
Malte Jörgens <malte.joergens@wps.de>
2021-12-03 06:18:05 +0100

    03: Input parsing

--- src/main/day03/Day03.kt
+++ src/main/day03/Day03.kt
@@ -2,11 +2,11 @@ package day03
 
 import readInput
 
-typealias Input = Any // TODO
+typealias Input = List<Int>
 typealias Output = Int
 
 /**
- * [TODO](https://adventofcode.com/2021/day/3)
+ * [Binary Diagnostic](https://adventofcode.com/2021/day/3)
  */
 fun main() {
     val input = readInput("day03", ::mapInput)
@@ -15,10 +15,12 @@ fun main() {
 }
 
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+fun mapInput(lines: Sequence<String>): Input = lines.map { it.toInt(2) }.toList()
 
 /**
- * TODO
+ * Use the binary numbers in your diagnostic report to calculate the gamma rate and epsilon rate,
+ * then multiply them together. **What is the power consumption of the submarine?**
+ * (Be sure to represent your answer in decimal, not binary.)
  */
 fun part1(input: Input): Output {
     TODO()
--- src/test/day03/Day03Test.kt
+++ src/test/day03/Day03Test.kt
@@ -4,12 +4,39 @@ import kotlin.test.*
 
 internal class Day03Test {
 
-    private val sampleInput: String = TODO()
+    private val sampleInput: String =
+        """00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"""
 
 
     @Test
     fun parseInput() {
-        assertEquals(TODO(), mapInput(sampleInput.lineSequence()))
+        assertEquals(
+            listOf(
+                0b00100,
+                0b11110,
+                0b10110,
+                0b10111,
+                0b10101,
+                0b01111,
+                0b00111,
+                0b11100,
+                0b10000,
+                0b11001,
+                0b00010,
+                0b01010
+            ), mapInput(sampleInput.lineSequence())
+        )
     }
 
     @Test
