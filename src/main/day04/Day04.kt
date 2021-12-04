608d7290487e9e6039b44c357f57447d2e6f7514
Malte Jörgens <malte@stbjoergens.de>
2021-12-04 09:11:42 +0100

    04: Input parsing

--- src/main/day04/Day04.kt
+++ src/main/day04/Day04.kt
@@ -2,11 +2,14 @@ package day04
 
 import readInput
 
-typealias Input = Any // TODO
+typealias Input = BingoGame
 typealias Output = Int
 
+data class BingoGame(val drawnNumbers: List<Int>, val bingoBoards: List<BingoBoard>)
+data class BingoBoard(val board: List<List<Int>>)
+
 /**
- * [TODO](https://adventofcode.com/2021/day/4)
+ * [Giant Squid](https://adventofcode.com/2021/day/4)
  */
 fun main() {
     val input = readInput("day04", ::mapInput)
@@ -15,18 +18,26 @@ fun main() {
 }
 
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+fun mapInput(lines: Sequence<String>): Input {
+    val allLines = lines.toList()
+    val drawnNumbers = allLines[0].split(',').map(String::toInt)
+    val boards = allLines.asIterable().drop(1).chunked(6) {
+        BingoBoard(parseMatrix(it.subList(1, 6), String::toInt))
+    }.toList()
+    return BingoGame(drawnNumbers, boards)
+}
 
 /**
- * TODO
+ * Parse a list of Strings each separated by whitespace into a Matrix applying the given transformation
  */
+fun <T> parseMatrix(list: List<String>, transform: (String) -> T, delimiter: Regex = Regex("[^\\w]+")): List<List<T>> {
+    return List(list.size) { list[it].trim().split(delimiter).map(transform = transform) }
+}
+
 fun part1(input: Input): Output {
     TODO()
 }
 
-/**
- * TODO
- */
 fun part2(input: Input): Output {
     TODO()
 }
