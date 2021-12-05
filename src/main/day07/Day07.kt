35253b7afb7fdbc2503452e13060eb23152deba3
Malte Jörgens <malte.joergens@wps.de>
2021-12-05 18:38:54 +0100

    07: Template

---
+++ src/main/day07/Day07.kt
@@ -0,0 +1,23 @@
+package day07
+
+import advent
+
+/**
+ * [TODO](https://adventofcode.com/2021/day/7)
+ */
+fun main() = advent("day07", ::mapInput, ::part1, ::part2)
+
+typealias Input = Any // TODO
+typealias Output = Int
+
+fun mapInput(lines: Sequence<String>): Input = TODO()
+
+fun part1(input: Input): Output {
+    TODO()
+}
+
+fun part2(input: Input): Output {
+    TODO()
+}
+
+
