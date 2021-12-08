e780e6ada4dfc85e871761b4570d0afa924a42f1
Malte Jörgens <malte.joergens@wps.de>
2021-12-08 07:46:18 +0100

    08: Part 1

--- src/main/day08/Day08.kt
+++ src/main/day08/Day08.kt
@@ -3,18 +3,26 @@ package day08
 import advent
 
 /**
- * [TODO](https://adventofcode.com/2021/day/8)
+ * [Seven Segment Search](https://adventofcode.com/2021/day/8)
  */
 fun main() = advent("day08", ::mapInput, ::part1, ::part2)
 
-typealias Input = Any // TODO
+typealias Input = List<Display>
 typealias Output = Int
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+data class Display(val patterns: List<String>, val outputValue: List<String>)
 
-fun part1(input: Input): Output {
-    TODO()
+fun mapInput(lines: Sequence<String>): Input {
+    val whitespace = Regex("\\W+")
+    return lines.map {
+        val (patternPart, outputPart) = it.split('|')
+        val patterns = patternPart.trim().split(whitespace)
+        val output = outputPart.trim().split(whitespace)
+        Display(patterns, output)
+    }.toList()
 }
+
+fun part1(input: Input): Output = input.sumOf { line -> line.outputValue.count { it.length in setOf(2, 3, 4, 7) } }
 
 fun part2(input: Input): Output {
     TODO()
--- src/test/day08/Day08Test.kt
+++ src/test/day08/Day08Test.kt
@@ -5,33 +5,49 @@ import kotlin.test.*
 
 internal class Day08Test {
 
-    private val sampleInput: String = TODO()
-
+    private val sampleInput: String =
+        """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
 
     @Test
     fun parseInput() {
-        assertEquals(TODO(), mapInput(sampleInput.lineSequence()))
+        assertEquals(
+            Display(
+                listOf("be", "cfbegad", "cbdgef", "fgaecd", "cgeb", "fdcge", "agebfd", "fecdb", "fabcd", "edb"),
+                listOf("fdgacbe", "cefdb", "cefbgd", "gcbe")
+            ), mapInput(sampleInput.lineSequence())[0]
+        )
     }
 
     @Test
     fun `part1 example`() {
         val input = mapInput(sampleInput.lineSequence())
-        assertEquals(TODO(), part1(input))
+        assertEquals(26, part1(input))
     }
 
     @Test
     fun part1() {
         val input = readInput("day08", ::mapInput)
-        assertEquals(TODO(), part1(input))
+        assertEquals(440, part1(input))
     }
 
     @Test
+    @Ignore
     fun `part2 example`() {
         val input = mapInput(sampleInput.lineSequence())
         assertEquals(TODO(), part2(input))
     }
 
     @Test
+    @Ignore
     fun part2() {
         val input = readInput("day08", ::mapInput)
         assertEquals(TODO(), part2(input))
