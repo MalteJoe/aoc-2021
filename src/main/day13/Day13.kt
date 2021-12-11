e02c62916d0e8cccd133ea7d31cec2d1675c6a3d
Malte Jörgens <malte@stbjoergens.de>
2021-12-11 12:13:24 +0100

    13: Template

---
+++ src/main/day13/Day13.kt
@@ -0,0 +1,15 @@
+package day13
+
+import advent
+
+/** [TODO](https://adventofcode.com/2021/day/13) */
+fun main() = advent("day13", ::mapInput, ::part1, ::part2)
+
+typealias Input = Any // TODO
+typealias Output = Int
+
+fun mapInput(lines: Sequence<String>): Input = TODO()
+
+fun part1(input: Input): Output = TODO()
+
+fun part2(input: Input): Output = TODO()
