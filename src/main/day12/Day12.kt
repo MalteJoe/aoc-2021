8ca22e054036413367b2261946ca64c211fa65ed
Malte Jörgens <malte@stbjoergens.de>
2021-12-12 09:01:58 +0100

    12: Part 1

--- src/main/day12/Day12.kt
+++ src/main/day12/Day12.kt
@@ -1,15 +1,39 @@
 package day12
 
 import advent
+import swap
 
-/** [TODO](https://adventofcode.com/2021/day/12) */
+/** [Passage Pathing](https://adventofcode.com/2021/day/12) */
 fun main() = advent("day12", ::mapInput, ::part1, ::part2)
 
-typealias Input = Any // TODO
+typealias Input = List<Connection>
 typealias Output = Int
+typealias Connection = Pair<Cave, Cave>
+typealias Cave = String
+typealias Path = List<Cave>
+typealias CaveMap = Map<Cave, List<Cave>>
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+fun mapInput(lines: Sequence<String>): Input {
+    val parser = Regex("""^(\w+)-(\w+)$""")
+    return lines.map {
+        val (from, to) = parser.find(it)?.destructured ?: error("Error parsing $it")
+        Pair(from, to)
+    }.toList()
+}
 
-fun part1(input: Input): Output = TODO()
+fun part1(input: Input): Output {
+    val map: CaveMap = input.zip(input.map(Connection::swap))
+        .flatMap { (forward, reverse) -> listOf(forward, reverse) }
+        .groupBy(Connection::first, Connection::second)
+    return numberOfPaths(map, listOf("start"))
+}
+
+fun numberOfPaths(map: CaveMap, explored: Path): Int {
+    val currentCave = explored.last()
+    if (currentCave == "end") return 1
+    return map[currentCave]!!.filterNot { it.isSmall() && it in explored }.sumOf { numberOfPaths(map, explored + it) }
+}
+
+fun Cave.isSmall(): Boolean = this == lowercase()
 
 fun part2(input: Input): Output = TODO()
--- src/test/day12/Day12Test.kt
+++ src/test/day12/Day12Test.kt
@@ -5,30 +5,75 @@ import kotlin.test.*
 
 internal class Day12Test {
 
-    private val sampleInput: String = TODO()
-
+    private val largerExample =
+        """fs-end
+          |he-DX
+          |fs-he
+          |start-DX
+          |pj-DX
+          |end-zg
+          |zg-sl
+          |zg-pj
+          |pj-he
+          |RW-he
+          |fs-DX
+          |pj-RW
+          |zg-RW
+          |start-pj
+          |he-WI
+          |zg-he
+          |pj-fs
+          |start-RW""".trimMargin()
 
     @Test
-    fun parseInput() {
-        assertEquals(TODO(), mapInput(sampleInput.lineSequence()))
+    fun `part1 short example`() {
+        val input = mapInput(
+            """start-A
+              |start-b
+              |A-c
+              |A-b
+              |b-d
+              |A-end
+              |b-end""".trimMargin().lineSequence()
+        )
+        assertEquals(10, part1(input))
     }
 
     @Test
-    fun `part1 example`() {
-        val input = mapInput(sampleInput.lineSequence())
-        assertEquals(TODO(), part1(input))
+    fun `part1 slightly larger example`() {
+        val input = mapInput(
+            """dc-end
+              |HN-start
+              |start-kj
+              |dc-start
+              |dc-HN
+              |LN-dc
+              |HN-end
+              |kj-sa
+              |kj-HN
+              |kj-dc""".trimMargin().lineSequence()
+        )
+        assertEquals(19, part1(input))
+    }
+
+    @Test
+    fun `part1 even larger example`() {
+        val input = mapInput(
+            largerExample.lineSequence()
+        )
+        assertEquals(226, part1(input))
     }
 
     @Test
     fun part1() {
         val input = readInput("day12", ::mapInput)
-        assertEquals(TODO(), part1(input))
+        assertEquals(4707, part1(input))
     }
 
     @Test
     @Ignore
     fun `part2 example`() {
-        val input = mapInput(sampleInput.lineSequence())
+        val input = mapInput(largerExample.lineSequence())
         assertEquals(TODO(), part2(input))
     }
 
