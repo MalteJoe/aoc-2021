577b660abd19cf47eda0d281c13a2e01d0291f98
Malte Jörgens <malte@stbjoergens.de>
2021-12-13 07:24:47 +0100

    14: Template

---
+++ src/test/day14/Day14Test.kt
@@ -0,0 +1,41 @@
+package day14
+
+import readInput
+import kotlin.test.*
+
+internal class Day14Test {
+
+    private val sampleInput: String = TODO()
+
+
+    @Test
+    fun parseInput() {
+        assertEquals(TODO(), mapInput(sampleInput.lineSequence()))
+    }
+
+    @Test
+    fun `part1 example`() {
+        val input = mapInput(sampleInput.lineSequence())
+        assertEquals(TODO(), part1(input))
+    }
+
+    @Test
+    fun part1() {
+        val input = readInput("day14", ::mapInput)
+        assertEquals(TODO(), part1(input))
+    }
+
+    @Test
+    @Ignore
+    fun `part2 example`() {
+        val input = mapInput(sampleInput.lineSequence())
+        assertEquals(TODO(), part2(input))
+    }
+
+    @Test
+    @Ignore
+    fun part2() {
+        val input = readInput("day14", ::mapInput)
+        assertEquals(TODO(), part2(input))
+    }
+}
