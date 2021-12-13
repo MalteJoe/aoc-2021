83c52949536ec98b88fa4a81cbb250bfe848a0a4
Malte Jörgens <malte@stbjoergens.de>
2021-12-13 08:02:27 +0100

    13: Parsing Input

--- src/main/day13/Day13.kt
+++ src/main/day13/Day13.kt
@@ -2,13 +2,26 @@ package day13
 
 import advent
 
-/** [TODO](https://adventofcode.com/2021/day/13) */
+/** [Transparent Origami](https://adventofcode.com/2021/day/13) */
 fun main() = advent("day13", ::mapInput, ::part1, ::part2)
 
-typealias Input = Any // TODO
+typealias Input = Pair<List<Dot>, List<Fold>>
 typealias Output = Int
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+data class Dot(val x: Int, val y: Int)
+data class Fold(val axis: Char, val line: Int)
+
+fun mapInput(lines: Sequence<String>): Input {
+    val allLines = lines.toList()
+    val splitAt = allLines.indexOf("")
+    val dots = allLines.subList(0, splitAt)
+        .map { it.split(',').map(String::toInt).let { (x, y) -> Dot(x, y) } }
+    val foldParser = Regex("""([xy])=(\d+)$""")
+    val folds = allLines.subList(splitAt + 1, allLines.size)
+        .mapNotNull { foldParser.find(it)?.destructured }
+        .map { (axis, line) -> Fold(axis[0], line.toInt()) }
+    return Pair(dots, folds)
+}
 
 fun part1(input: Input): Output = TODO()
 
--- src/test/day13/Day13Test.kt
+++ src/test/day13/Day13Test.kt
@@ -5,18 +5,42 @@ import kotlin.test.*
 
 internal class Day13Test {
 
-    private val sampleInput: String = TODO()
+    private val sampleInput: String = """6,10
+          |0,14
+          |9,10
+          |0,3
+          |10,4
+          |4,11
+          |6,0
+          |6,12
+          |4,1
+          |0,13
+          |10,12
+          |3,4
+          |3,0
+          |8,4
+          |1,10
+          |2,14
+          |8,10
+          |9,0
+
+          |fold along y=7
+          |fold along x=5
+""".trimMargin()
 
 
     @Test
     fun parseInput() {
-        assertEquals(TODO(), mapInput(sampleInput.lineSequence()))
+        val mappedInput = mapInput(sampleInput.lineSequence())
+        assertEquals(18, mappedInput.first.size)
+        assertEquals(listOf(Dot(6, 10), Dot(0, 14), Dot(9, 10)), mappedInput.first.slice(0..2))
+        assertEquals(listOf(Fold('y', 7), Fold('x', 5)), mappedInput.second)
     }
 
     @Test
     fun `part1 example`() {
         val input = mapInput(sampleInput.lineSequence())
-        assertEquals(TODO(), part1(input))
+        assertEquals(17, part1(input))
     }
 
     @Test
