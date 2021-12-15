512919a0832c2a1c53004c87e1b0e39b7907e1ca
Malte Jörgens <malte@stbjoergens.de>
2021-12-15 08:14:53 +0100

    15: Part 1

--- src/main/day15/Day15.kt
+++ src/main/day15/Day15.kt
@@ -1,15 +1,40 @@
 package day15
 
+import Matrix
 import advent
+import charList
+import java.util.*
+import kotlin.collections.LinkedHashMap
 
-/** [TODO](https://adventofcode.com/2021/day/15) */
+/** [Chiton](https://adventofcode.com/2021/day/15) */
 fun main() = advent("day15", ::mapInput, ::part1, ::part2)
 
-typealias Input = Any // TODO
+typealias Input = Matrix<Int>
 typealias Output = Int
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+data class Dijkstra<T>(val cost: Int, val previous: T?)
+typealias Node = Matrix.Coordinate
 
-fun part1(input: Input): Output = TODO()
+fun mapInput(lines: Sequence<String>): Input = Matrix(lines.map { it.charList(Char::digitToInt) }.toList())
+
+fun part1(input: Input): Output {
+    val minimalRisks = LinkedHashMap<Node, Dijkstra<Node>>().apply { put(Node(0, 0), Dijkstra(0, null)) }
+    val toExplore = PriorityQueue<Pair<Node, Dijkstra<Node>>>(compareBy { it.second.cost })
+    var currentCost = 0
+    val target = Node(input.cols - 1, input.rows - 1)
+    var expand = minimalRisks.keys.last()
+    while (expand != target) {
+        toExplore.addAll(input.neighbours(expand, diagonal = false)
+            .filter { it !in minimalRisks }
+            .map { Pair(it, Dijkstra(currentCost + input[it], expand)) })
+
+        val next = toExplore.poll()
+        minimalRisks[next.first] = next.second
+        toExplore.removeIf { it.first == next.first }
+        currentCost = next.second.cost
+        expand = minimalRisks.keys.last()
+    }
+    return minimalRisks.values.last().cost
+}
 
 fun part2(input: Input): Output = TODO()
--- src/test/day15/Day15Test.kt
+++ src/test/day15/Day15Test.kt
@@ -5,24 +5,28 @@ import kotlin.test.*
 
 internal class Day15Test {
 
-    private val sampleInput: String = TODO()
-
-
-    @Test
-    fun parseInput() {
-        assertEquals(TODO(), mapInput(sampleInput.lineSequence()))
-    }
+    private val sampleInput: String =
+        """1163751742
+          |1381373672
+          |2136511328
+          |3694931569
+          |7463417111
+          |1319128137
+          |1359912421
+          |3125421639
+          |1293138521
+          |2311944581""".trimMargin()
 
     @Test
     fun `part1 example`() {
         val input = mapInput(sampleInput.lineSequence())
-        assertEquals(TODO(), part1(input))
+        assertEquals(40, part1(input))
     }
 
     @Test
     fun part1() {
         val input = readInput("day15", ::mapInput)
-        assertEquals(TODO(), part1(input))
+        assertEquals(619, part1(input))
     }
 
     @Test
