e938d8df4a7f9ca1434b3d35114dcb0339de575b
Malte Jörgens <malte.joergens@wps.de>
2021-12-16 22:09:01 +0100

    16: Part 1

--- src/main/day16/Day16.kt
+++ src/main/day16/Day16.kt
@@ -1,15 +1,75 @@
 package day16
 
 import advent
+import java.io.*
 
-/** [TODO](https://adventofcode.com/2021/day/16) */
+/** [Packet Decoder](https://adventofcode.com/2021/day/16) */
 fun main() = advent("day16", ::mapInput, ::part1, ::part2)
 
-typealias Input = Any // TODO
+typealias Input = String
 typealias Output = Int
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+interface Packet {
+    val version: Int
+    val type: Int
+    val subPackets: List<Packet>
+}
 
-fun part1(input: Input): Output = TODO()
+data class Literal(override val version: Int, val value: Long) : Packet {
+    override val type: Int get() = 4
+    override val subPackets: List<Packet> get() = emptyList()
+}
+
+data class Operator(
+    override val version: Int,
+    override val type: Int,
+    override val subPackets: List<Packet>
+) : Packet
+
+fun mapInput(lines: Sequence<String>): Input = lines.first()
+
+fun String.hexToBinary() = map { it.digitToInt(16).toString(2).padStart(4, '0') }.joinToString("")
+fun Packet.versionSum(): Int = version + subPackets.sumOf(Packet::versionSum)
+
+fun part1(input: Input): Output = parsePacket(input.hexToBinary().reader()).versionSum()
+
+fun parsePacket(bitstream: StringReader): Packet {
+    val version = bitstream.read(3).toInt(2)
+    val type = bitstream.read(3).toInt(2)
+    if (type == 4) return Literal(version, parseLiteralValue(bitstream))
+    val subPackets = if (bitstream.read() == '0'.code) {
+        val bitLength = bitstream.read(15).toInt(2)
+        parseSubpackets(bitstream, bitLength)
+    } else {
+        val numberOfPackets = bitstream.read(11).toInt(2)
+        parseNSubpackets(bitstream, numberOfPackets)
+    }
+    return Operator(version, type, subPackets)
+}
+
+private fun StringReader.read(count: Int): String = String(CharArray(count).let { read(it); it })
+
+fun parseSubpackets(bitstream: StringReader, length: Int): List<Packet> {
+    val subPacketReader = StringReader(bitstream.read(length))
+    val subPackets = mutableListOf<Packet>()
+    while (subPacketReader.read() != -1) {
+        subPacketReader.reset()
+        subPackets.add(parsePacket(subPacketReader))
+        subPacketReader.mark(0)
+    }
+    return subPackets
+}
+
+fun parseNSubpackets(bitstream: StringReader, count: Int): List<Packet> = (0 until count).map { parsePacket(bitstream) }
+
+fun parseLiteralValue(bitstream: StringReader): Long {
+    var block: String
+    val builder = StringBuilder()
+    do {
+        block = bitstream.read(5)
+        builder.append(block.substring(1))
+    } while (block[0] != '0')
+    return builder.toString().toLong(2)
+}
 
 fun part2(input: Input): Output = TODO()
--- src/test/day16/Day16Test.kt
+++ src/test/day16/Day16Test.kt
@@ -5,18 +5,49 @@ import kotlin.test.*
 
 internal class Day16Test {
 
-    private val sampleInput: String = TODO()
-
-
     @Test
-    fun parseInput() {
-        assertEquals(TODO(), mapInput(sampleInput.lineSequence()))
+    fun `parse literal`() {
+        val input = "D2FE28".hexToBinary().reader()
+
+        val packet = parsePacket(input)
+
+        assertEquals(6, packet.version)
+        assertEquals(4, packet.type)
+        assertEquals(0, packet.subPackets.size)
+        assertTrue(packet is Literal)
+        assertEquals(2021, packet.value)
     }
 
     @Test
-    fun `part1 example`() {
-        val input = mapInput(sampleInput.lineSequence())
-        assertEquals(TODO(), part1(input))
+    fun `parse operator by bits`() {
+        val input = "38006F45291200".hexToBinary().reader()
+
+        val packet = parsePacket(input)
+
+        assertEquals(1, packet.version)
+        assertEquals(6, packet.type)
+        assertEquals(2, packet.subPackets.size)
+        assertTrue(packet is Operator)
+    }
+
+    @Test
+    fun `parse operator by packet count`() {
+        val input = "EE00D40C823060".hexToBinary().reader()
+
+        val packet = parsePacket(input)
+
+        assertEquals(7, packet.version)
+        assertEquals(3, packet.type)
+        assertEquals(3, packet.subPackets.size)
+        assertTrue(packet is Operator)
+    }
+
+    @Test
+    fun `part1 examples`() {
+        assertEquals(16, part1("8A004A801A8002F478"))
+        assertEquals(12, part1("620080001611562C8802118E34"))
+        assertEquals(23, part1("C0015000016115A2E0802F182340"))
+        assertEquals(31, part1("A0016C880162017C3686B18A3D4780"))
     }
 
     @Test
@@ -28,8 +59,8 @@ internal class Day16Test {
     @Test
     @Ignore
     fun `part2 example`() {
-        val input = mapInput(sampleInput.lineSequence())
-        assertEquals(TODO(), part2(input))
+        //val input = mapInput(sampleInput.lineSequence())
+        //assertEquals(TODO(), part2(input))
     }
 
     @Test
