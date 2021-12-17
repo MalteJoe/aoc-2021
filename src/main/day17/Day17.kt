17fc16245ca2f0338e537a0b19204d29205d3c13
Malte Jörgens <malte@stbjoergens.de>
2021-12-17 08:44:52 +0100

    17: Part 2 refactored

--- src/main/day17/Day17.kt
+++ src/main/day17/Day17.kt
@@ -1,7 +1,7 @@
 package day17
 
 import advent
-import java.lang.Math.max
+import kotlin.math.sign
 
 /** [Trick Shot](https://adventofcode.com/2021/day/17) */
 fun main() = advent("day17", ::mapInput, ::part1, ::part2)
@@ -15,19 +15,17 @@ data class Area(val xRange: IntRange, val yRange: IntRange) {
 
 data class Vector(val x: Int, val y: Int) {
     fun hitsTarget(area: Area): Boolean {
-        var xPos = x
-        var yPos = y
-        var xVelocity = x
-        var yVelocity = y
-        while (xPos <= area.xRange.last && yPos >= area.yRange.first) {
-            if (Vector(xPos, yPos) in area) return true
-            xVelocity = (xVelocity-1).coerceAtLeast(0)
-            yVelocity -= 1
-            xPos += xVelocity
-            yPos += yVelocity
+        var pos = this
+        var velocity = this
+        while (pos.x <= area.xRange.last && pos.y >= area.yRange.first) {
+            if (pos in area) return true
+            velocity += Vector(-velocity.x.sign, -1)
+            pos += velocity
         }
         return false
     }
+
+    operator fun plus(vector: Vector): Vector = Vector(x + vector.x, y + vector.y)
 
 }
 
@@ -40,7 +38,7 @@ fun mapInput(lines: Sequence<String>): Input {
 
 fun part1(input: Input): Output {
     // x coordinates are irrelevant for part 1.
-    // We always want to shoot up, at some point yVelocity becomes 0 and visits the same y values on it's
+    // We always want to shoot up, at some point yVelocity becomes 0 and visits the same y values on its
     // way back down. So the "best" shot is reaching the target within 1 step after having same y as the origin.
     // Max Y is calculated by the triangular number formula
     return input.yRange.first * (input.yRange.first + 1) / 2
