30e9dab490ef7d82626b2f32fb2c8c5a6454ff97
Malte Jörgens <malte@stbjoergens.de>
2021-12-18 16:00:23 +0100

    18: Part 1

--- src/main/day18/Day18.kt
+++ src/main/day18/Day18.kt
@@ -2,6 +2,7 @@ package day18
 
 import util.advent
 import java.io.StringReader
+import kotlin.math.sign
 
 /** [Snailfish](https://adventofcode.com/2021/day/18) */
 fun main() = advent("day18", ::mapInput, ::part1, ::part2)
@@ -9,13 +10,10 @@ fun main() = advent("day18", ::mapInput, ::part1, ::part2)
 typealias Input = List<Number>
 typealias Output = Int
 
-interface Value
-data class Number(val left: Value, val right: Value) : Value
-data class Regular(val value: Int) : Value
-
 fun mapInput(lines: Sequence<String>): Input = lines.map(String::reader).map(::parseNumber).toList()
 
-fun part1(input: Input): Output = TODO()
+fun part1(input: Input): Output = input.sum().magnitude()
+fun Input.sum() = reduce { acc, number -> (acc + number).reduced() }
 
 fun part2(input: Input): Output = TODO()
 
@@ -36,4 +34,74 @@ fun parseValue(reader: StringReader): Value {
 fun StringReader.peek(): Char {
     mark(0)
     return read().toChar().also { reset() }
+}
+
+
+interface Value {
+    fun magnitude(): Int
+    operator fun plus(value: Value): Value
+    fun split(): Number?
+}
+
+data class Number(val left: Value, val right: Value) : Value {
+    constructor(left: Int, right: Value) : this(Regular(left), right)
+    constructor(left: Value, right: Int) : this(left, Regular(right))
+    constructor(left: Int, right: Int) : this(Regular(left), Regular(right))
+
+    fun reduced(): Number {
+        exploded()?.let { return it.reduced() }
+        split()?.let { return it.reduced() }
+        return this
+    }
+
+    fun exploded(): Number? = exploded(0)?.newNumber as Number?
+    override fun split() = left.split()?.let { copy(left = it) } ?: right.split()?.let { copy(right = it) }
+
+    private fun exploded(depth: Int): Explosion? {
+        if (depth == 4) {
+            return Explosion(left as Regular, Regular(0), right as Regular)
+        }
+        if (left is Number) {
+            val explosion = left.exploded(depth + 1)
+            if (explosion != null) {
+                val newNumber = if (explosion.right == null) copy(left = explosion.newNumber)
+                else Number(explosion.newNumber, explosion.right + right)
+                return explosion.copy(newNumber = newNumber, right = null)
+            }
+        }
+        if (right is Number) {
+            val explosion = right.exploded(depth + 1)
+            if (explosion != null) {
+                val newNumber = if (explosion.left == null) copy(right = explosion.newNumber)
+                else Number(left + explosion.left, explosion.newNumber)
+                return explosion.copy(newNumber = newNumber, left = null)
+            }
+        }
+        return null
+    }
+
+    override fun plus(value: Value): Number = when (value) {
+        is Regular -> copy(right = right + value)
+        is Number -> Number(this, value)
+        else -> error("unknown value $value")
+    }
+
+    override fun toString(): String = "[$left,$right]"
+
+    override fun magnitude(): Int = 3 * left.magnitude() + 2 * right.magnitude()
+
+    private data class Explosion(val left: Regular?, val newNumber: Value, val right: Regular?)
+}
+
+data class Regular(val value: Int) : Value {
+    override fun magnitude() = value
+    override fun plus(value: Value): Value = when (value) {
+        is Regular -> Regular(this.value + value.value)
+        is Number -> value.copy(left = this + value.left)
+        else -> error("unknown value $value")
+    }
+
+    override fun split(): Number? = if (value >= 10) Number(value / 2, value / 2 + value.rem(2).sign) else null
+
+    override fun toString(): String = value.toString()
 }
--- src/test/day18/Day18KtTest.kt
+++ src/test/day18/Day18KtTest.kt
@@ -5,10 +5,6 @@ import kotlin.test.*
 
 internal class Day18KtTest {
 
-    private fun Number(left: Int, right: Int) = Number(Regular(left), Regular(right))
-    private fun Number(left: Number, right: Int) = Number(left, Regular(right))
-    private fun Number(left: Int, right: Number) = Number(Regular(left), right)
-
     @Test
     fun parseInput() {
         val input = mapInput("[9,[8,7]]\n[[[[1,2],[3,4]],[[5,6],[7,8]]],9]".lineSequence())
@@ -19,16 +15,137 @@ internal class Day18KtTest {
                 Number(
                     Number(Number(1, 2), Number(3, 4)),
                     Number(Number(5, 6), Number(7, 8))
-                ), 9
-            ), input[1]
+                ),
+                9
+            ),
+            input[1]
         )
     }
 
     @Test
-    @Ignore
+    fun `explode left`() {
+        val input = parseNumber("[[[[[9,8],1],2],3],4]".reader())
+        val expected = parseNumber("[[[[0,9],2],3],4]".reader())
+        assertEquals(expected, input.exploded())
+    }
+
+    @Test
+    fun `explode right`() {
+        val input = parseNumber("[7,[6,[5,[4,[3,2]]]]]".reader())
+        val expected = parseNumber("[7,[6,[5,[7,0]]]]".reader())
+        assertEquals(expected, input.exploded())
+    }
+
+    @Test
+    fun `explode middle`() {
+        val input = parseNumber("[[6,[5,[4,[3,2]]]],1]".reader())
+        val expected = parseNumber("[[6,[5,[7,0]]],3]".reader())
+        assertEquals(expected, input.exploded())
+    }
+
+    @Test
+    fun `explode middle only once`() {
+        val input = parseNumber("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]".reader())
+        val expected = parseNumber("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]".reader())
+        assertEquals(expected, input.exploded())
+    }
+
+    @Test
+    fun split() {
+        assertEquals(null, Regular(5).split())
+        assertEquals(Number(5, 5), Regular(10).split())
+        assertEquals(Number(5, 6), Regular(11).split())
+        assertEquals(Number(Number(5, 6), 12), Number(11, 12).split())
+        assertEquals(Number(5, Number(6, 6)), Number(5, 12).split())
+    }
+
+    @Test
+    fun `part1 small examples`() {
+        var input = mapInput("[[[[4,3],4],4],[7,[[8,4],9]]]\n[1,1]".lineSequence())
+        assertEquals(parseNumber("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]".reader()), input.sum())
+
+        input = mapInput(
+            """[1,1]
+              |[2,2]
+              |[3,3]
+              |[4,4]""".trimMargin().lineSequence()
+        )
+        assertEquals(parseNumber("[[[[1,1],[2,2]],[3,3]],[4,4]]".reader()), input.sum())
+
+        input = mapInput(
+            """[1,1]
+              |[2,2]
+              |[3,3]
+              |[4,4]
+              |[5,5]""".trimMargin().lineSequence()
+        )
+        assertEquals(parseNumber("[[[[3,0],[5,3]],[4,4]],[5,5]]".reader()), input.sum())
+
+        input = mapInput(
+            """[1,1]
+              |[2,2]
+              |[3,3]
+              |[4,4]
+              |[5,5]
+              |[6,6]""".trimMargin().lineSequence()
+        )
+        assertEquals(parseNumber("[[[[5,0],[7,4]],[5,5]],[6,6]]".reader()), input.sum())
+    }
+
+    @Test
+    fun `part1 larger example`() {
+        val input = mapInput(
+            """[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+              |[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+              |[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+              |[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+              |[7,[5,[[3,8],[1,4]]]]
+              |[[2,[2,2]],[8,[8,1]]]
+              |[2,9]
+              |[1,[[[9,3],9],[[9,0],[0,7]]]]
+              |[[[5,[7,4]],7],1]
+              |[[[[4,2],2],6],[8,7]]""".trimMargin().lineSequence()
+        )
+        val finalNumber = input.sum()
+        assertEquals(parseNumber("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]".reader()), finalNumber)
+    }
+
+    @Test
+    fun magnitude() {
+        assertEquals(29, parseNumber("[9,1]".reader()).magnitude())
+        assertEquals(21, parseNumber("[1,9]".reader()).magnitude())
+        assertEquals(129, parseNumber("[[9,1],[1,9]]".reader()).magnitude())
+        assertEquals(143, parseNumber("[[1,2],[[3,4],5]]".reader()).magnitude())
+        assertEquals(1384, parseNumber("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]".reader()).magnitude())
+        assertEquals(445, parseNumber("[[[[1,1],[2,2]],[3,3]],[4,4]]".reader()).magnitude())
+        assertEquals(791, parseNumber("[[[[3,0],[5,3]],[4,4]],[5,5]]".reader()).magnitude())
+        assertEquals(1137, parseNumber("[[[[5,0],[7,4]],[5,5]],[6,6]]".reader()).magnitude())
+        assertEquals(3488, parseNumber("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]".reader()).magnitude())
+    }
+
+    @Test
+    fun `part1 example`() {
+        val input = mapInput(
+            """[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+              |[[[5,[2,8]],4],[5,[[9,9],0]]]
+              |[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+              |[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+              |[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+              |[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+              |[[[[5,4],[7,7]],8],[[8,3],8]]
+              |[[9,3],[[9,9],[6,[4,9]]]]
+              |[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+              |[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]""".trimMargin().lineSequence()
+        )
+        val finalNumber = input.sum()
+        assertEquals(parseNumber("[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]".reader()), finalNumber)
+        assertEquals(4140, finalNumber.magnitude())
+    }
+
+    @Test
     fun part1() {
         val input = readInput("day18", ::mapInput)
-        assertEquals(TODO(), part1(input))
+        assertEquals(4137, part1(input))
     }
 
     @Test
