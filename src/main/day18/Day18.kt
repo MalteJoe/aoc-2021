03b4c0711c6de3d75573376682a6531b24bda682
Malte Jörgens <malte@stbjoergens.de>
2021-12-18 08:24:01 +0100

    18: Parse Input

--- src/main/day18/Day18.kt
+++ src/main/day18/Day18.kt
@@ -1,15 +1,39 @@
 package day18
 
 import util.advent
+import java.io.StringReader
 
-/** [TODO](https://adventofcode.com/2021/day/18) */
+/** [Snailfish](https://adventofcode.com/2021/day/18) */
 fun main() = advent("day18", ::mapInput, ::part1, ::part2)
 
-typealias Input = Any // TODO
+typealias Input = List<Number>
 typealias Output = Int
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+interface Value
+data class Number(val left: Value, val right: Value) : Value
+data class Regular(val value: Int) : Value
+
+fun mapInput(lines: Sequence<String>): Input = lines.map(String::reader).map(::parseNumber).toList()
 
 fun part1(input: Input): Output = TODO()
 
 fun part2(input: Input): Output = TODO()
+
+fun parseNumber(reader: StringReader): Number {
+    assert(reader.read() == '['.code)
+    val left = parseValue(reader)
+    assert(reader.read() == ','.code)
+    val right = parseValue(reader)
+    assert(reader.read() == ']'.code)
+    return Number(left, right)
+}
+
+fun parseValue(reader: StringReader): Value {
+    return if (reader.peek() == '[') parseNumber(reader)
+    else Regular(reader.read().toChar().digitToInt())
+}
+
+fun StringReader.peek(): Char {
+    mark(0)
+    return read().toChar().also { reset() }
+}
--- src/test/day18/Day18KtTest.kt
+++ src/test/day18/Day18KtTest.kt
@@ -3,34 +3,32 @@ package day18
 import util.readInput
 import kotlin.test.*
 
-@Ignore
 internal class Day18KtTest {
 
-    private val sampleInput: String = TODO()
-
+    private fun Number(left: Int, right: Int) = Number(Regular(left), Regular(right))
+    private fun Number(left: Number, right: Int) = Number(left, Regular(right))
+    private fun Number(left: Int, right: Number) = Number(Regular(left), right)
 
     @Test
     fun parseInput() {
-        assertEquals(TODO(), mapInput(sampleInput.lineSequence()))
-    }
-
-    @Test
-    fun `part1 example`() {
-        val input = mapInput(sampleInput.lineSequence())
-        assertEquals(TODO(), part1(input))
-    }
-
-    @Test
-    fun part1() {
-        val input = readInput("day18", ::mapInput)
-        assertEquals(TODO(), part1(input))
+        val input = mapInput("[9,[8,7]]\n[[[[1,2],[3,4]],[[5,6],[7,8]]],9]".lineSequence())
+        assertEquals(2, input.size)
+        assertEquals(Number(9, Number(8, 7)), input[0])
+        assertEquals(
+            Number(
+                Number(
+                    Number(Number(1, 2), Number(3, 4)),
+                    Number(Number(5, 6), Number(7, 8))
+                ), 9
+            ), input[1]
+        )
     }
 
     @Test
     @Ignore
-    fun `part2 example`() {
-        val input = mapInput(sampleInput.lineSequence())
-        assertEquals(TODO(), part2(input))
+    fun part1() {
+        val input = readInput("day18", ::mapInput)
+        assertEquals(TODO(), part1(input))
     }
 
     @Test
