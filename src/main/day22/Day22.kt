06ebd491a52d53bae8189b27089b3f39051cd16b
Malte Jörgens <malte@stbjoergens.de>
2021-12-21 19:24:51 +0100

    22: Template

---
+++ src/main/day22/Day22.kt
@@ -0,0 +1,15 @@
+package day22
+
+import util.advent
+
+/** [TODO](https://adventofcode.com/2021/day/22) */
+fun main() = advent("day22", ::mapInput, ::part1, ::part2)
+
+typealias Input = Any // TODO
+typealias Output = Int
+
+fun mapInput(lines: Sequence<String>): Input = TODO()
+
+fun part1(input: Input): Output = TODO()
+
+fun part2(input: Input): Output = TODO()
