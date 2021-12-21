894610aedd30db4ae0441932ce077d05af7e2689
Malte Jörgens <malte@stbjoergens.de>
2021-12-21 19:25:47 +0100

    23: Template

---
+++ src/main/day23/Day23.kt
@@ -0,0 +1,15 @@
+package day23
+
+import util.advent
+
+/** [TODO](https://adventofcode.com/2021/day/23) */
+fun main() = advent("day23", ::mapInput, ::part1, ::part2)
+
+typealias Input = Any // TODO
+typealias Output = Int
+
+fun mapInput(lines: Sequence<String>): Input = TODO()
+
+fun part1(input: Input): Output = TODO()
+
+fun part2(input: Input): Output = TODO()
---
+++ src/test/day23/Day23KtTest.kt
@@ -0,0 +1,42 @@
+package day23
+
+import util.readInput
+import kotlin.test.*
+
+@Ignore
+internal class Day23KtTest {
+
+    private val sampleInput: String = TODO()
+
+
+    @Test
+    fun parseInput() {
+        assertEquals(TODO(), mapInput(sampleInput.lineSequence()))
+    }
+
+    @Test
+    fun `part1 example`() {
+        val input = mapInput(sampleInput.lineSequence())
+        assertEquals(TODO(), part1(input))
+    }
+
+    @Test
+    fun part1() {
+        val input = readInput("day23", ::mapInput)
+        assertEquals(TODO(), part1(input))
+    }
+
+    @Test
+    @Ignore
+    fun `part2 example`() {
+        val input = mapInput(sampleInput.lineSequence())
+        assertEquals(TODO(), part2(input))
+    }
+
+    @Test
+    @Ignore
+    fun part2() {
+        val input = readInput("day23", ::mapInput)
+        assertEquals(TODO(), part2(input))
+    }
+}
