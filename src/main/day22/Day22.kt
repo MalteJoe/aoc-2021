16dabf2731cf1643fc766475e58026db2c868add
Malte Jörgens <malte@stbjoergens.de>
2021-12-22 13:28:06 +0100

    22: Part 1

--- src/main/day22/Day22.kt
+++ src/main/day22/Day22.kt
@@ -1,15 +1,55 @@
 package day22
 
-import util.advent
+import util.*
+import kotlin.math.*
 
-/** [TODO](https://adventofcode.com/2021/day/22) */
+/** [Reactor Reboot](https://adventofcode.com/2021/day/22) */
 fun main() = advent("day22", ::mapInput, ::part1, ::part2)
 
-typealias Input = Any // TODO
-typealias Output = Int
+typealias Input = List<Step>
+typealias Output = Long
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+data class Step(val on: Boolean, val cuboid: Cuboid)
+typealias Cuboid = Vector<IntRange>
 
-fun part1(input: Input): Output = TODO()
+fun mapInput(lines: Sequence<String>): Input {
+    val parser = Regex("""(on|off) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)""")
+    return lines.map { line ->
+        val groups = parser.find(line)?.groupValues ?: error("Error parsing $line")
+        val cuboid = Vector(groups.subList(2, 8).map(String::toInt).chunked(2) { (from, to) -> from..to })
+        Step(groups[1] == "on", cuboid)
+    }.toList()
+}
+
+fun part1(input: Input): Output {
+    val part1Area = Cuboid(-50..50, -50..50, -50..50)
+    return normalizeSteps(input.filter { part1Area.intersect(it.cuboid) != null }).sumOf { it.cuboid.cubeCount * if (it.on) 1 else -1 }
+}
+
+private fun normalizeSteps(input: Input): MutableList<Step> {
+    val normalizedSteps = mutableListOf<Step>()
+    for (step in input) {
+        // Insert steps to turn invert the intersection volumes
+        normalizedSteps += normalizedSteps.mapNotNull { previous ->
+            previous.cuboid.intersect(step.cuboid)?.let { Step(!previous.on, it) }
+        }
+        if (step.on) normalizedSteps.add(step)
+    }
+    return normalizedSteps
+}
+
+
+internal fun Cuboid.intersect(cuboid: Cuboid): Cuboid? {
+    if (!(x.overlaps(cuboid.x) && y.overlaps(cuboid.y) && z.overlaps(cuboid.z))) return null
+    return Cuboid(
+        max(x.first, cuboid.x.first)..min(x.last, cuboid.x.last),
+        max(y.first, cuboid.y.first)..min(y.last, cuboid.y.last),
+        max(z.first, cuboid.z.first)..min(z.last, cuboid.z.last)
+    )
+}
+
+private fun IntRange.overlaps(other: IntRange): Boolean = first <= other.last && last >= other.first
+private val IntRange.size: Int get() = last - start + 1
+private val Cuboid.cubeCount: Long get() = x.size.toLong() * y.size * z.size
 
 fun part2(input: Input): Output = TODO()
