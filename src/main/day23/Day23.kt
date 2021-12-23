141445ee1c14a44a6981adb8e06899a1b1246dbc
Malte Jörgens <malte@stbjoergens.de>
2021-12-23 16:32:08 +0100

    23: Part 1

--- src/main/day23/Day23.kt
+++ src/main/day23/Day23.kt
@@ -1,15 +1,93 @@
 package day23
 
 import util.advent
+import java.math.BigInteger.TEN
+import java.util.*
+import kotlin.math.*
 
-/** [TODO](https://adventofcode.com/2021/day/23) */
+/** [Amphipod](https://adventofcode.com/2021/day/23) */
 fun main() = advent("day23", ::mapInput, ::part1, ::part2)
 
-typealias Input = Any // TODO
+typealias Input = List<String>
 typealias Output = Int
 
-fun mapInput(lines: Sequence<String>): Input = TODO()
+data class State(val hallway: String, val rooms: Map<Char, String>)
 
-fun part1(input: Input): Output = TODO()
+fun mapInput(lines: Sequence<String>): Input = lines.toList()
+
+val targetState: State = State(".".repeat(11), mapOf('A' to "AA", 'B' to "BB", 'C' to "CC", 'D' to "DD"))
+
+fun part1(input: Input): Output {
+    val start = input.toState()
+    val explored = mutableSetOf<State>()
+    val toExplore = PriorityQueue<Pair<State, Int>>(compareBy { it.second })
+    var next = Pair(start, 0)
+    while (next.first != targetState) {
+        if (explored.add(next.first)) {
+            toExplore += possibleMoves(next.first).map { it.copy(second = it.second + next.second) }
+        }
+
+        next = toExplore.poll()
+    }
+    return next.second
+}
 
 fun part2(input: Input): Output = TODO()
+
+internal fun List<String>.toState(): State {
+    return State(get(1).substring(1 until 12), "ABCD".associateWith { room ->
+        val col = room.hallwayIndex + 1
+        (2..3).map { get(it)[col] }.joinToString("")
+    })
+}
+
+internal fun possibleMoves(state: State): List<Pair<State, Int>> {
+    val newStates = mutableListOf<Pair<State, Int>>()
+    // moves from hallway
+    state.hallway.mapIndexedNotNullTo(newStates) { index, c ->
+        if (!c.isAmphipod) return@mapIndexedNotNullTo null
+        // Once an amphipod stops moving in the hallway, it will stay in that spot until it can move into a room.
+        // Amphipods will never move from the hallway into a room unless that room is their destination room and
+        // that room contains no amphipods which do not also have that room as their own destination.
+        val hallwayIndex = c.hallwayIndex
+        val hallwayToDoor =
+            state.hallway.substring(if (index < hallwayIndex) index + 1..hallwayIndex else hallwayIndex until index)
+        if (hallwayToDoor.any(Char::isAmphipod) || state.rooms[c]!!.any { it.isAmphipod && it != c }) {
+            return@mapIndexedNotNullTo null
+        }
+        val rooms = state.rooms.toMutableMap()
+        val newPos = rooms[c]!!.lastIndexOf('.')
+        rooms[c] = rooms[c]!!.withReplacement(newPos, c)
+        val cost = (newPos + 1 + hallwayToDoor.length) * c.movementCost
+        return@mapIndexedNotNullTo Pair(State(state.hallway.withReplacement(index, '.'), rooms), cost)
+    }
+
+    // moves from rooms
+    state.rooms.flatMapTo(newStates) { (room, roomPositions) ->
+        // perfect position
+        if (roomPositions.all { it == room || it == '.' }) return@flatMapTo emptyList()
+        // only the topmost amphipod can move
+        val oldPos = roomPositions.indexOfFirst(Char::isAmphipod)
+        val newRooms = state.rooms.toMutableMap()
+        newRooms[room] = roomPositions.withReplacement(oldPos, '.')
+        // Amphipods will never stop on the space immediately outside any room.
+        listOf(0, 1, 3, 5, 7, 9, 10).filterNot {
+            state.hallway.substring(
+                min(it, room.hallwayIndex),
+                max(it + 1, room.hallwayIndex)
+            ).any(Char::isAmphipod)
+        }.map { targetPos ->
+            val cost = (oldPos + 1 + abs(targetPos - room.hallwayIndex)) * roomPositions[oldPos].movementCost
+            Pair(State(state.hallway.withReplacement(targetPos, roomPositions[oldPos]), newRooms), cost)
+        }
+    }
+
+    return newStates
+}
+
+private fun String.withReplacement(index: Int, value: Char): String =
+    toCharArray().also { it[index] = value }.concatToString()
+
+private val Char.isAmphipod: Boolean get() = this in "ABCD"
+private val Char.hallwayIndex: Int get() = (this - 'A') * 2 + 2
+private val Char.movementCost: Int get() = TEN.pow(this - 'A').toInt()
--- src/test/day23/Day23KtTest.kt
+++ src/test/day23/Day23KtTest.kt
@@ -3,27 +3,105 @@ package day23
 import util.readInput
 import kotlin.test.*
 
-@Ignore
 internal class Day23KtTest {
 
-    private val sampleInput: String = TODO()
-
+    private val sampleInput: String = """
+        #############
+        #...........#
+        ###B#C#B#D###
+          #A#D#C#A#
+          #########""".trimIndent()
 
     @Test
-    fun parseInput() {
-        assertEquals(TODO(), mapInput(sampleInput.lineSequence()))
+    fun mapState() {
+        assertEquals(
+            State("...........", mapOf('A' to "BA", 'B' to "CD", 'C' to "BC", 'D' to "DA")),
+            mapInput(sampleInput.lineSequence()).toState()
+        )
+    }
+
+    @Test
+    fun `example step by step`() {
+        // Starting configuration
+        var state = mapInput(sampleInput.lineSequence()).toState()
+        // One Bronze amphipod moves into the hallway, taking 4 steps and using 40 energy
+        var possibleMoves = possibleMoves(state)
+        var expected = State("...B.......", mapOf('A' to "BA", 'B' to "CD", 'C' to ".C", 'D' to "DA"))
+        assertTrue(Pair(expected, 40) in possibleMoves)
+        state = expected
+
+        // The only Copper amphipod not in its side room moves there, taking 4 steps and using 400 energy
+        possibleMoves = possibleMoves(state)
+        expected = State("...B.C.....", mapOf('A' to "BA", 'B' to ".D", 'C' to ".C", 'D' to "DA"))
+        assertTrue(Pair(expected, 200) in possibleMoves)
+        state = expected
+
+        possibleMoves = possibleMoves(state)
+        expected = State("...B.......", mapOf('A' to "BA", 'B' to ".D", 'C' to "CC", 'D' to "DA"))
+        assertTrue(Pair(expected, 200) in possibleMoves)
+        state = expected
+
+        // A Desert amphipod moves out of the way, taking 3 steps and using 3000 energy, ...
+        possibleMoves = possibleMoves(state)
+        expected = State("...B.D.....", mapOf('A' to "BA", 'B' to "..", 'C' to "CC", 'D' to "DA"))
+        assertTrue(Pair(expected, 3000) in possibleMoves)
+        state = expected
+
+        // ... and then the Bronze amphipod takes its place, taking 3 steps and using 30 energy
+        possibleMoves = possibleMoves(state)
+        expected = State(".....D.....", mapOf('A' to "BA", 'B' to ".B", 'C' to "CC", 'D' to "DA"))
+        assertTrue(Pair(expected, 30) in possibleMoves)
+        state = expected
+
+        // The leftmost Bronze amphipod moves to its room using 40 energy:
+        possibleMoves = possibleMoves(state)
+        expected = State("...B.D.....", mapOf('A' to ".A", 'B' to ".B", 'C' to "CC", 'D' to "DA"))
+        assertTrue(Pair(expected, 20) in possibleMoves)
+        state = expected
+
+        possibleMoves = possibleMoves(state)
+        expected = State(".....D.....", mapOf('A' to ".A", 'B' to "BB", 'C' to "CC", 'D' to "DA"))
+        assertTrue(Pair(expected, 20) in possibleMoves)
+        state = expected
+
+        // Both amphipods in the rightmost room move into the hallway, using 2003 energy in total:
+        possibleMoves = possibleMoves(state)
+        expected = State(".....D.D...", mapOf('A' to ".A", 'B' to "BB", 'C' to "CC", 'D' to ".A"))
+        assertTrue(Pair(expected, 2000) in possibleMoves)
+        state = expected
+
+        possibleMoves = possibleMoves(state)
+        expected = State(".....D.D.A.", mapOf('A' to ".A", 'B' to "BB", 'C' to "CC", 'D' to ".."))
+        assertTrue(Pair(expected, 3) in possibleMoves)
+        state = expected
+
+        // Both Desert amphipods move into the rightmost room using 7000 energy:
+        possibleMoves = possibleMoves(state)
+        expected = State(".....D...A.", mapOf('A' to ".A", 'B' to "BB", 'C' to "CC", 'D' to ".D"))
+        assertTrue(Pair(expected, 3000) in possibleMoves)
+        state = expected
+
+        possibleMoves = possibleMoves(state)
+        expected = State(".........A.", mapOf('A' to ".A", 'B' to "BB", 'C' to "CC", 'D' to "DD"))
+        assertTrue(Pair(expected, 4000) in possibleMoves)
+        state = expected
+
+        // Finally, the last Amber amphipod moves into its room, using 8 energy
+        possibleMoves = possibleMoves(state)
+        expected = State("...........", mapOf('A' to "AA", 'B' to "BB", 'C' to "CC", 'D' to "DD"))
+        assertTrue(Pair(expected, 8) in possibleMoves)
     }
 
     @Test
     fun `part1 example`() {
         val input = mapInput(sampleInput.lineSequence())
-        assertEquals(TODO(), part1(input))
+        assertEquals(12521, part1(input))
     }
 
     @Test
     fun part1() {
         val input = readInput("day23", ::mapInput)
-        assertEquals(TODO(), part1(input))
+        assertEquals(19046, part1(input))
     }
 
     @Test
